77aa37eb79c1a1c1072fc045a46e9cf38ecb639f
William Burns <mudokonman@gmail.com>
2015-09-15 12:11:35 -0400

    Updated streams to use some of the newer functions and CacheCollectors

--- streams/src/main/java/org/infinispan/tutorial/simple/streams/InfinispanStreams.java
+++ streams/src/main/java/org/infinispan/tutorial/simple/streams/InfinispanStreams.java
@@ -3,10 +3,12 @@ package org.infinispan.tutorial.simple.streams;
 import java.io.Serializable;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.stream.CacheCollectors;
 
 public class InfinispanStreams {
 
@@ -14,14 +16,14 @@ public class InfinispanStreams {
       // Construct a simple local cache manager with default configuration
       DefaultCacheManager cacheManager = new DefaultCacheManager();
       // Obtain the default cache
-      Cache<Integer, String> cache = cacheManager.getCache();
+      Cache<String, String> cache = cacheManager.getCache();
       // Store some values
       int range = 10;
-      IntStream.range(0, range).boxed().forEach(i -> cache.put(i, i + "-value"));
+      IntStream.range(0, range).boxed().forEach(i -> cache.put(i + "-key", i + "-value"));
       // Map and reduce the keys
-      Integer result = cache.keySet().stream()
-         .map((Serializable & Function<Integer, Integer>) e -> e)
-         .reduce(0, (Serializable & BinaryOperator<Integer>) (e1, e2) -> e1 + e2);
+      int result = cache.keySet().stream()
+         .map((Serializable & Function<String, Integer>) e -> Integer.valueOf(e.substring(0, e.indexOf("-"))))
+              .collect(CacheCollectors.serializableCollector(() -> Collectors.summingInt(i -> i.intValue())));
       System.out.printf("Result = %d\n", result);
       // Stop the cache manager and release all resources
       cacheManager.stop();
