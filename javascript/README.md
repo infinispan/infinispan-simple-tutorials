cd30b4faeccc4ac3382325418344f706a62e1254
Galder Zamarreño <galder@zamarreno.com>
2015-12-02 17:48:53 +0100

    Add Javascript tutorial

---
+++ javascript/README.md
@@ -0,0 +1,14 @@
+# Build requirements
+
+* Node.js
+* Infinispan Server 8.1.x or higher 
+
+# Build instructions
+
+    npm install
+
+# Run instructions
+
+* Start Infinispan server calling `./bin/standalone.sh` (Linux, OS X, Unix) or 
+`bin/standalone.bat` (Windows).
+* Execute: `node index.js`
---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+var infinispan = require('infinispan');
+
+var connected = infinispan.client(11222, '127.0.0.1');
+connected.then(function(client) {
+  console.log("Connected");
+  var putGetPromise = client.put('key', 'value').then(function () {
+    return client.get('key').then(function (value) {
+      console.log('key = ' + value);
+    })
+  });
+
+  return putGetPromise.finally(function() {
+    // Regardless of the result, disconnect client
+    return client.disconnect().then(function() { console.log("Disconnected") });
+  });
+});
