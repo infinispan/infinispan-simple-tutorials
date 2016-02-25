1595531dd7dc2bff1ba9f158f2b1e9a1e7ee6352
Sebastian Laskawiec <slaskawi@redhat.com>
2016-02-25 13:44:56 +0100

    Spring examples

---
+++ spring/src/main/java/org/infinispan/tutorial/simple/spring/SpringCaching.java
@@ -0,0 +1,61 @@
+package org.infinispan.tutorial.simple.spring;
+
+import org.infinispan.spring.provider.SpringEmbeddedCacheManagerFactoryBean;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This example shows how to use Spring's {@link Cacheable} annotation on slow methods.
+ */
+public class SpringCaching {
+
+    @Configuration
+    @EnableCaching
+    public static class SpringConfiguration {
+
+        @Bean
+        public SpringEmbeddedCacheManagerFactoryBean springCache() {
+            return new SpringEmbeddedCacheManagerFactoryBean();
+        }
+
+        @Bean
+        public CachedObject cachedObject() {
+            return new CachedObject();
+        }
+    }
+
+    public static class CachedObject {
+
+        @Cacheable(value = "default")
+        public String verySlowMethod() {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return "Spring and Infinispan will speed this one up!";
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+
+        CachedObject cachePlayground = applicationContext.getBean(CachedObject.class);
+
+        printWithTime(() -> cachePlayground.verySlowMethod());
+        printWithTime(() -> cachePlayground.verySlowMethod());
+    }
+
+    private static void printWithTime(Callable<String> functionToCall) throws Exception {
+        long startTime = System.currentTimeMillis();
+        String result = functionToCall.call();
+        System.out.println("Returned: \"" + result + "\" in " + (System.currentTimeMillis() - startTime) / 1000 + " s");
+    }
+}
