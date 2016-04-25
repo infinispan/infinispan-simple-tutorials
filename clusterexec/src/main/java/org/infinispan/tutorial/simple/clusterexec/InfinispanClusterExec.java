3cd7600ebf7f33e462b46a26f20e2b739738fbc5
William Burns <mudokonman@gmail.com>
2016-04-25 14:42:43 -0400

    ClusterExecutor example that mirrors dist exec

---
+++ clusterexec/src/main/java/org/infinispan/tutorial/simple/clusterexec/InfinispanClusterExec.java
@@ -0,0 +1,31 @@
+package org.infinispan.tutorial.simple.clusterexec;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.distexec.DefaultExecutorService;
+import org.infinispan.manager.ClusterExecutor;
+import org.infinispan.manager.DefaultCacheManager;
+
+public class InfinispanClusterExec {
+
+   public static void main(String[] args) {
+      // Setup up a clustered cache manager
+      GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
+      // Initialize the cache manager
+      DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
+      ClusterExecutor clusterExecutor = cacheManager.executor();
+      clusterExecutor.submitConsumer(cm -> new Random().nextInt(), (address, intValue, exception) ->
+              System.out.printf("%s\n", intValue));
+      // Shuts down the cache manager and all associated resources
+      cacheManager.stop();
+   }
+
+}
