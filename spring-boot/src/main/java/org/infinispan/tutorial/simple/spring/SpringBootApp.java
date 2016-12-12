08586a952e7aac60cfff67d18f1d999c2c2b2062
Sebastian Laskawiec <slaskawi@redhat.com>
2016-12-12 12:28:51 +0100

    ISPN-6561 Spring Boot Starter demo for Client/Server

--- spring-boot/src/main/java/org/infinispan/tutorial/simple/spring/SpringBootApp.java
+++ spring-boot/src/main/java/org/infinispan/tutorial/simple/spring/SpringBootApp.java
@@ -2,21 +2,13 @@ package org.infinispan.tutorial.simple.spring;
 
 import java.lang.invoke.MethodHandles;
 
-import org.infinispan.Cache;
-import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.configuration.cache.Configuration;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.configuration.global.GlobalConfigurationBuilder;
-import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import infinispan.autoconfigure.embedded.InfinispanCacheConfigurer;
-import infinispan.autoconfigure.embedded.InfinispanGlobalConfigurer;
 
 /**
  * This example shows how to configure Spring Boot and Infinispan
@@ -26,44 +18,16 @@ import infinispan.autoconfigure.embedded.InfinispanGlobalConfigurer;
 @SpringBootApplication
 public class SpringBootApp {
 
-    private static final String CACHE_NAME = "test";
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    /**
-     * This bean is optional but it shows how to inject {@link org.infinispan.configuration.global.GlobalConfiguration}.
-     */
-    @Bean
-    public InfinispanGlobalConfigurer globalConfiguration() {
-        logger.info("Defining Global Configuration");
-        return () -> GlobalConfigurationBuilder
-              .defaultClusteredBuilder()
-              .globalJmxStatistics().allowDuplicateDomains(true)
-              .build();
-    }
-
-    /**
-     * Here we inject {@link Configuration}.
-     */
-    @Bean
-    public InfinispanCacheConfigurer cacheConfigurer() {
-        logger.info("Defining {} configuration", CACHE_NAME);
-        return manager -> {
-            Configuration ispnConfig = new ConfigurationBuilder()
-                  .clustering().cacheMode(CacheMode.DIST_SYNC)
-                  .build();
-
-            manager.defineConfiguration(CACHE_NAME, ispnConfig);
-        };
-    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SpringBootApp.class, args);
 
-        EmbeddedCacheManager cacheManager = ctx.getBean(EmbeddedCacheManager.class);
-        Cache<Long, String> cache = cacheManager.getCache(CACHE_NAME);
+        RemoteCacheManager cacheManager = ctx.getBean(RemoteCacheManager.class);
+        RemoteCache<Long, String> cache = cacheManager.getCache();
         cache.put(System.currentTimeMillis(), "Infinispan");
 
-        logger.info("Values from Cache: {}", cache.entrySet());
+        logger.info("Values from Cache: {}", cache.getBulk());
     }
 
 }
