834115daa28c3c8a556e7351b66f9a5a2844afeb
Katia Aresti <karesti@redhat.com>
2021-06-11 12:50:24 +0200

    ISPN-12512 Fix some issues
    * Package name with - have query parse errors
    * For the console, it's less confusing to use Key in String

--- spring-integration/spring-boot/remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasquesNamesSchemaBuilder.java
+++ spring-integration/spring-boot/remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/BasquesNamesSchemaBuilder.java
@@ -3,6 +3,6 @@ package org.infinispan.tutorial.simple.spring.remote;
 import org.infinispan.protostream.GeneratedSchema;
 import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 
-@AutoProtoSchemaBuilder(schemaPackageName = "simple-tutorials", includeClasses = BasqueName.class)
+@AutoProtoSchemaBuilder(schemaPackageName = "tutorial", includeClasses = BasqueName.class)
 public interface BasquesNamesSchemaBuilder extends GeneratedSchema {
 }
