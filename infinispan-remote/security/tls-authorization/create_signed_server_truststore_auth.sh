6065407a156a3b40a506190aee69b0710c56d78d
Pedro Ruivo <pruivo@redhat.com>
2021-08-17 14:56:29 +0100

    Use keytool from JAVA_HOME if available
    Downstream, we use JAVA_HOME to set the JDK to use and Maven uses it as
    well to compile; old keytool version (Java 8) blocks waiting for
    user input which fails the downstream Jenkins jobs.

--- infinispan-remote/security/tls-authorization/create_signed_server_truststore_auth.sh
+++ infinispan-remote/security/tls-authorization/create_signed_server_truststore_auth.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+if [ -f ${JAVA_HOME}/bin/keytool ] ; then
+  # Maven uses JAVA_HOME to compile the project. If keytool bin is there, use it instead
+  KEYTOOL_CMD="${JAVA_HOME}/bin/keytool"
+else
+  # Otherwise, uses the keytool defined in path.
+  KEYTOOL_CMD="keytool"
+fi
+
 # First create a server trust store using create-signed_certificates.sh
 # Make a copy of the server trust store
 cp server_truststore.p12 server_truststoreAuth.p12
 
 # Import the client certificate into the server trust store
 # This is necessary if you use client certificate authentication
-keytool -importcert -alias client1 -keystore server_truststoreAuth.p12 -file client1.cer -storepass ServerTrustsecret -noprompt
-keytool -importcert -alias client2 -keystore server_truststoreAuth.p12 -file client2.cer -storepass ServerTrustsecret -noprompt
+${KEYTOOL_CMD} -importcert -alias client1 -keystore server_truststoreAuth.p12 -file client1.cer -storepass ServerTrustsecret -noprompt
+${KEYTOOL_CMD} -importcert -alias client2 -keystore server_truststoreAuth.p12 -file client2.cer -storepass ServerTrustsecret -noprompt
