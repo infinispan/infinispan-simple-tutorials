821439e7a3a9031c33eedff19d9005724435e55a
Katia Aresti <karesti@redhat.com>
2022-10-11 16:28:03 +0200

    ISPN-14214 Redis connector simple tutorial

---
+++ infinispan-remote/redis-client/src/main/java/org/infinispan/tutorial/simple/redis/RedisClientCache.java
@@ -0,0 +1,23 @@
+package org.infinispan.tutorial.simple.redis;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.infinispan.tutorial.simple.connect.Infinispan;
+
+public class RedisClientCache {
+
+   public static void main(String[] args) {
+      String redisUri = String.format("redis://%s:%s@%s:%s",
+            Infinispan.USER,
+            Infinispan.PASSWORD,
+            Infinispan.HOST,
+            Infinispan.SINGLE_PORT);
+      RedisClient client = RedisClient.create(redisUri);
+      RedisCommands<String, String> redisCommands = client.connect().sync();
+      String key = "Hello";
+      redisCommands.set(key, "world");
+      String value = redisCommands.get(key);
+      System.out.println(String.format("Read from Infinispan using a Redis Client (Resp Protocol): %s %s", key, value));
+   }
+
+}
