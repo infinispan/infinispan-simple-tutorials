f9ebbb725e8dfc597baba8e7048da2fc23254cdd
Katia Aresti <karesti@redhat.com>
2022-10-19 16:52:51 +0200

    ISPN-13358 Cross site tutorial for 14.0 * Simplify docker-compose * Use the operator with minikube

--- infinispan-remote/cross-site-replication/docker-compose/create-data.sh
+++ infinispan-remote/cross-site-replication/docker-compose/create-data.sh
@@ -1,18 +1,21 @@
 echo "= Init create data"
 echo "= Clear all data"
-curl -XDELETE  -u use:pass http://localhost:11222/rest/v2/caches/xsiteCache
-curl -XDELETE  -u use:pass http://localhost:31222/rest/v2/caches/xsiteCache
+curl -XDELETE --digest -u admin:password http://localhost:11222/rest/v2/caches/xsiteCache
+curl -XDELETE --digest -u admin:password http://localhost:31222/rest/v2/caches/xsiteCache
 
 echo "= Create X-Site Cache"
-curl -XPOST  -u use:pass -H "Content-Type: application/xml" -d "@xsiteCache.xml" http://localhost:11222/rest/v2/caches/xsiteCache
-curl -XPOST  -u use:pass -H "Content-Type: application/xml" -d "@ny-backup-xsiteCache.xml" http://localhost:31222/rest/v2/caches/xsiteCache
+curl -XPOST --digest -u admin:password -H "Content-Type: application/xml" -d "@xsiteCache.xml" http://localhost:11222/rest/v2/caches/xsiteCache
+curl -XPOST --digest -u admin:password -H "Content-Type: application/xml" -d "@ny-backup-xsiteCache.xml" http://localhost:31222/rest/v2/caches/xsiteCache
 
 echo "= Put 10 entries in XSite cache"
 for i in {1..10}
 do
-  URL="http://localhost:11222/rest/v2/caches/xsiteCache/$i"
-  DATA="data-$i"
-  curl -XPOST  -u use:pass -d $DATA $URL
+  URL='http://localhost:11222/rest/v2/caches/xsiteCache/key_'$i
+  DATA='{
+             "_type": "string",
+             "_value": "value_'$i'"
+          }'
+  curl -XPOST --digest -u admin:password -d "$DATA" -H 'Content-Type: application/json' $URL
 done
 
 echo "= End"
