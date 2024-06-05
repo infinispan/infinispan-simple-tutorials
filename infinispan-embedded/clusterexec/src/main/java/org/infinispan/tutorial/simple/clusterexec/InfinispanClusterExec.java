5b0c185eff21d21c18d7b4f6a9651a7b0a4bf140
Katia Aresti <karesti@redhat.com>
2024-06-05 17:19:30 +0200

    ISPN-16160 Unit Test Clustered Executor

--- infinispan-embedded/clusterexec/src/main/java/org/infinispan/tutorial/simple/clusterexec/InfinispanClusterExec.java
+++ infinispan-embedded/clusterexec/src/main/java/org/infinispan/tutorial/simple/clusterexec/InfinispanClusterExec.java
@@ -1,30 +1,44 @@
 package org.infinispan.tutorial.simple.clusterexec;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-
-import org.infinispan.Cache;
-import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.ClusterExecutor;
 import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.remoting.transport.Address;
+import org.infinispan.util.function.SerializableFunction;
+import org.infinispan.util.function.TriConsumer;
+
+import java.util.Random;
 
 public class InfinispanClusterExec {
 
+   DefaultCacheManager cacheManager;
+
    public static void main(String[] args) {
+      InfinispanClusterExec infinispanClusterExec = new InfinispanClusterExec();
+      infinispanClusterExec.createCacheManager();
+      infinispanClusterExec.submitTask(cm -> new Random().nextInt(),
+              (address, intValue, exception) -> System.out.printf("%s\n", intValue));
+      infinispanClusterExec.stopDefaultCacheManager();
+   }
+
+   public void submitTask(SerializableFunction<EmbeddedCacheManager, Object> task, TriConsumer<Address, Object, Throwable> triConsumer) {
+      ClusterExecutor clusterExecutor = cacheManager.executor();
+      clusterExecutor.submitConsumer(task, triConsumer);
+   }
+
+   public void createCacheManager() {
       // Setup up a clustered cache manager
       GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
       // Initialize the cache manager
-      DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
-      ClusterExecutor clusterExecutor = cacheManager.executor();
-      clusterExecutor.submitConsumer(cm -> new Random().nextInt(), (address, intValue, exception) ->
-              System.out.printf("%s\n", intValue));
-      // Shuts down the cache manager and all associated resources
-      cacheManager.stop();
+      this.cacheManager = new DefaultCacheManager(global.build());
    }
 
+   public void stopDefaultCacheManager() {
+      if (cacheManager != null) {
+         // Shuts down the cache manager and all associated resources
+         cacheManager.stop();
+         cacheManager = null;
+      }
+   }
 }
