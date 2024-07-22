dfb4ac38d09e6ef256244d7f32b687b391a100f0
Katia Aresti <karesti@redhat.com>
2024-07-22 15:29:53 +0200

    ISPN-16160 Unit Test Replicated

--- infinispan-embedded/cache-replicated/src/main/java/org/infinispan/tutorial/simple/replicated/InfinispanReplicated.java
+++ infinispan-embedded/cache-replicated/src/main/java/org/infinispan/tutorial/simple/replicated/InfinispanReplicated.java
@@ -12,32 +12,48 @@ import org.infinispan.manager.DefaultCacheManager;
 import java.util.UUID;
 
 public class InfinispanReplicated {
+   static DefaultCacheManager cacheManager;
+   static Cache<String, String> cache;
 
    public static void main(String[] args) throws Exception {
+      InfinispanReplicated.createDefaultCacheManagerAndStartCache();
+      InfinispanReplicated.manipulateReplicatedCache();
+      InfinispanReplicated.stopDefaultCacheManager();
+   }
+
+   public static void createDefaultCacheManagerAndStartCache() {
       // Setup up a clustered cache manager
       GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
       // Initialize the cache manager
-      DefaultCacheManager cacheManager = new DefaultCacheManager(global.build());
+      cacheManager = new DefaultCacheManager(global.build());
       // Create a replicated synchronous configuration
       ConfigurationBuilder builder = new ConfigurationBuilder();
       builder.clustering().cacheMode(CacheMode.REPL_SYNC);
       Configuration cacheConfig = builder.build();
       // Create a cache
-      Cache<String, String> cache = cacheManager.administration()
+      cache = cacheManager.administration()
               .withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
               .getOrCreateCache("cache", cacheConfig);
+   }
 
+   public static void manipulateReplicatedCache() {
       // Store the current node address in some random keys
       for(int i=0; i < 10; i++) {
          cache.put(UUID.randomUUID().toString(), cacheManager.getNodeAddress());
       }
+
       // Display the current cache contents for the whole cluster
       cache.entrySet().forEach(entry -> System.out.printf("%s = %s\n", entry.getKey(), entry.getValue()));
+
       // Display the current cache contents for this node
       cache.getAdvancedCache().withFlags(Flag.SKIP_REMOTE_LOOKUP)
-         .entrySet().forEach(entry -> System.out.printf("%s = %s\n", entry.getKey(), entry.getValue()));
-      // Stop the cache manager and release all resources
-      cacheManager.stop();
+              .entrySet().forEach(entry -> System.out.printf("%s = %s\n", entry.getKey(), entry.getValue()));
    }
 
+   public static void stopDefaultCacheManager() {
+      if (cacheManager != null) {
+         // Stop the cache manager and release all resources
+         cacheManager.stop();
+      }
+   }
 }
