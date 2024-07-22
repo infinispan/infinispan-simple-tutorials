6c4e321f5b6d00f83fb3fb5922ce784424d56956
Katia Aresti <karesti@redhat.com>
2024-07-22 16:00:41 +0200

    ISPN-16160 Unit Test Query

--- infinispan-embedded/query/src/main/java/org/infinispan/tutorial/simple/query/InfinispanQuery.java
+++ infinispan-embedded/query/src/main/java/org/infinispan/tutorial/simple/query/InfinispanQuery.java
@@ -1,6 +1,5 @@
 package org.infinispan.tutorial.simple.query;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.infinispan.Cache;
@@ -13,32 +12,47 @@ import org.infinispan.manager.EmbeddedCacheManager;
 
 public class InfinispanQuery {
 
-   public static void main(String[] args) throws IOException {
+   static Cache<String, Person> cache;
+   static EmbeddedCacheManager cacheManager;
+
+   public static void main(String[] args) throws Exception {
+      createCacheManagerAndCache();
+      List<Person> matches = addDataAndPerformQuery();
+      // Display results
+      matches.forEach(person -> System.out.printf("Match: %s%n", person));
+      stopCacheManager();
+   }
+
+   static List<Person> addDataAndPerformQuery() {
+      // Store some entries
+      cache.put("person1", new Person("William", "Shakespeare"));
+      cache.put("person2", new Person("William", "Wordsworth"));
+      cache.put("person3", new Person("John", "Milton"));
+      // Construct a query
+      Query<Person> query = cache.query("from org.infinispan.tutorial.simple.query.Person where name = 'William'");
+      // Execute the query
+      return query.execute().list();
+   }
+
+   static EmbeddedCacheManager createCacheManagerAndCache() {
       // Create cache manager
-      try (EmbeddedCacheManager cacheManager = new DefaultCacheManager()) {
+      cacheManager = new DefaultCacheManager();
+      // Create cache config
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.indexing()
+              .enable()
+              .storage(IndexStorage.LOCAL_HEAP)
+              .addIndexedEntity(Person.class);
 
-         // Create cache config
-         ConfigurationBuilder builder = new ConfigurationBuilder();
-         builder.indexing()
-               .enable()
-               .storage(IndexStorage.LOCAL_HEAP)
-               .addIndexedEntity(Person.class);
+      // Obtain the cache
+      cache = cacheManager.administration()
+              .withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
+              .getOrCreateCache("cache", builder.build());
+      return cacheManager;
+   }
 
-         // Obtain the cache
-         Cache<String, Person> cache = cacheManager.administration()
-               .withFlags(CacheContainerAdmin.AdminFlag.VOLATILE)
-               .getOrCreateCache("cache", builder.build());
-
-         // Store some entries
-         cache.put("person1", new Person("William", "Shakespeare"));
-         cache.put("person2", new Person("William", "Wordsworth"));
-         cache.put("person3", new Person("John", "Milton"));
-         // Construct a query
-         Query<Person> query = cache.query("from org.infinispan.tutorial.simple.query.Person where name = 'William'");
-         // Execute the query
-         List<Person> matches = query.execute().list();
-         // List the results
-         matches.forEach(person -> System.out.printf("Match: %s%n", person));
+   static void stopCacheManager() {
+      if (cacheManager != null) {
          // Stop the cache manager and release all resources
          cacheManager.stop();
       }
