06cc5b022300c6452f400040946934c3681655d3
Katia Aresti <karesti@redhat.com>
2024-07-23 10:27:19 +0200

    ISPN-16160 Unit Test Local Map

--- infinispan-embedded/map/src/main/java/org/infinispan/tutorial/simple/map/InfinispanMap.java
+++ infinispan-embedded/map/src/main/java/org/infinispan/tutorial/simple/map/InfinispanMap.java
@@ -6,19 +6,33 @@ import org.infinispan.manager.DefaultCacheManager;
 
 public class InfinispanMap {
 
+   static DefaultCacheManager cacheManager;
+   static Cache<String, String> cache;
+
    public static void main(String[] args) {
-      // Construct a simple local cache manager with default configuration
-      DefaultCacheManager cacheManager = new DefaultCacheManager();
-      // Define local cache configuration
-      cacheManager.defineConfiguration("local", new ConfigurationBuilder().build());
-      // Obtain the local cache
-      Cache<String, String> cache = cacheManager.getCache("local");
+      createAndStartComponents();
+      manipulateLocalCache();
+      stop();
+   }
+
+   static void manipulateLocalCache() {
       // Store a value
       cache.put("key", "value");
       // Retrieve the value and print it out
       System.out.printf("key = %s\n", cache.get("key"));
+   }
+
+   static void createAndStartComponents() {
+      // Construct a simple local cache manager with default configuration
+      cacheManager = new DefaultCacheManager();
+      // Define local cache configuration
+      cacheManager.defineConfiguration("local", new ConfigurationBuilder().build());
+      // Obtain the local cache
+      cache = cacheManager.getCache("local");
+   }
+
+   static void stop() {
       // Stop the cache manager and release all resources
       cacheManager.stop();
    }
-
 }
