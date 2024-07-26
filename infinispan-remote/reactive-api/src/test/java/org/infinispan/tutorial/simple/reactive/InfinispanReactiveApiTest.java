24a00d094efd992114d8f4e6f5cf25edb68896d8
Katia Aresti <karesti@redhat.com>
2024-07-26 09:30:20 +0200

    ISPN-16317 Fix Reactive tutorial

--- infinispan-remote/reactive-api/src/test/java/org/infinispan/tutorial/simple/reactive/InfinispanReactiveApiTest.java
+++ infinispan-remote/reactive-api/src/test/java/org/infinispan/tutorial/simple/reactive/InfinispanReactiveApiTest.java
@@ -2,12 +2,14 @@ package org.infinispan.tutorial.simple.reactive;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper.TUTORIAL_CACHE_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Disabled
 public class InfinispanReactiveApiTest {
 
     @BeforeAll
