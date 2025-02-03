fa10114ddaca0eafbc0a0fac01b5953623e60936
Katia Aresti <karesti@redhat.com>
2025-02-03 10:20:35 +0100

    Updates to 15.1.15 and cleans BASIC intelligence

--- integrations/spring-boot/cache-remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/InfinispanConfiguration.java
+++ integrations/spring-boot/cache-remote/src/main/java/org/infinispan/tutorial/simple/spring/remote/InfinispanConfiguration.java
@@ -1,8 +1,6 @@
 package org.infinispan.tutorial.simple.spring.remote;
 
-import org.infinispan.client.hotrod.configuration.ClientIntelligence;
 import org.infinispan.commons.marshall.ProtoStreamMarshaller;
-import org.infinispan.commons.util.OS;
 import org.infinispan.spring.starter.remote.InfinispanRemoteCacheCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,12 +31,6 @@ public class InfinispanConfiguration {
 
          // Add marshaller in the client, the class is generated from the interface in compile time
          b.addContextInitializer(new BasquesNamesSchemaBuilderImpl());
-
-         // #### This is to avoid connectivity issues locally with docker and mac.
-         // ### Don't use client intelligence basic in production
-         if (OS.getCurrentOs().equals(OS.MAC_OS) || OS.getCurrentOs().equals(OS.WINDOWS)) {
-            b.clientIntelligence(ClientIntelligence.BASIC);
-         }
       };
    }
 }
