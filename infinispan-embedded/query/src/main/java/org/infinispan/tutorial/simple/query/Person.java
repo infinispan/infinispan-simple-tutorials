030889cff6c459b35c39859b8bce0ac0110caabc
Katia Aresti <karesti@redhat.com>
2025-09-29 10:59:16 +0200

    Uses Common annotations in Indexed caches

--- infinispan-embedded/query/src/main/java/org/infinispan/tutorial/simple/query/Person.java
+++ infinispan-embedded/query/src/main/java/org/infinispan/tutorial/simple/query/Person.java
@@ -1,15 +1,14 @@
 package org.infinispan.tutorial.simple.query;
 
-
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+import org.infinispan.api.annotations.indexing.Basic;
+import org.infinispan.api.annotations.indexing.Indexed;
 
 @Indexed
 public class Person {
-   @KeywordField
+   @Basic
    String name;
 
-   @KeywordField
+   @Basic
    String surname;
 
    public Person(String name, String surname) {
