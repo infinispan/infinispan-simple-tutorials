a242818465a83f20c02f5add93e85962d9908865
Katia Aresti <karestig@ibm.com>
2026-03-19 09:37:08 +0100

    Support for Kotlin

--- infinispan-remote/kotlin/src/main/kotlin/org/infinispan/tutorial/simple/kotlin/InfinispanKotlinExample.kt
+++ infinispan-remote/kotlin/src/main/kotlin/org/infinispan/tutorial/simple/kotlin/InfinispanKotlinExample.kt
@@ -6,8 +6,8 @@ import org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper
 import org.infinispan.tutorial.simple.connect.TutorialsConnectorHelper.TUTORIAL_CACHE_NAME
 
 object InfinispanKotlinExample {
-    private lateinit var cacheManager: RemoteCacheManager
-    private lateinit var cache: RemoteCache<String, String>
+    lateinit var cacheManager: RemoteCacheManager
+    lateinit var cache: RemoteCache<String, String>
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -16,21 +16,21 @@ object InfinispanKotlinExample {
         disconnect()
     }
 
-    private fun manipulateCache() {
+    fun manipulateCache() {
         // Store a value
         cache.put("key", "value")
         // Retrieve the value and print it out
         println("key = ${cache["key"]}")
     }
 
-    private fun connectToInfinispan() {
+    fun connectToInfinispan() {
         // Connect to the server
         cacheManager = TutorialsConnectorHelper.connect()
         // Obtain the remote cache
         cache = cacheManager.getCache(TUTORIAL_CACHE_NAME)
     }
 
-    private fun disconnect() {
+    fun disconnect() {
         // Stop the cache manager and release all resources
         TutorialsConnectorHelper.stop(cacheManager)
     }
